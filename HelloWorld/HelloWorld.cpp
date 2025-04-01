a0e075c921bfe6359e76697f81e9899597676f39
Notaweedle <165727447+Notaweedle@users.noreply.github.com>
2025-04-01 14:20:58 -0600

    DataTypes.cpp

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <Datatypes.h>
+
 
 using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
     cout << "Git is now tracking this project!" << endl;
-    DataTypes() pest ;
-    pest().dataTypes;
+
     return 0;
 }
