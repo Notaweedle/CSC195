10c51bd50ecc915a381ced113a7393dcaae2ca1c
Notaweedle <165727447+Notaweedle@users.noreply.github.com>
2025-04-01 14:13:24 -0600

    Stuff woo

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <Datatypes.h>
+
 using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
     cout << "Git is now tracking this project!" << endl;
+    DataTypes() pest ;
+    pest().dataTypes;
     return 0;
 }
---
+++ HelloWorld/plink.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rmajka on 4/1/2025.
+//
+
+#include "plink.h"
